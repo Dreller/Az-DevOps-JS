f03d3756d99b6439cefc21a8f8bb85f7f71985df
Dreller <60242965+Dreller@users.noreply.github.com>
2023-03-26 22:34:35 +0000

    Save

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html", title="ADO JS Interaction", org="dreller08xp1", pat="rkriqbbxnebqzrdhmydcn762ueue6qdlvht7qixxvn4gbrdqwhsq")
